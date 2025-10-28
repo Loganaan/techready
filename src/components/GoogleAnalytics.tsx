dd38e6493bab5741c7fa44657ae151356de0f68a
Christian Ramirez <christianrc7325@gmail.com>
2025-10-28 10:37:40 -0500

    changed to correct tag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 export default function GoogleAnalytics() {
-  const GA_MEASUREMENT_ID = 'G-Q5MDTEYM4F';
+  const GA_MEASUREMENT_ID = 'G-PERJ0LSWS6';
 
   return (
     <>
